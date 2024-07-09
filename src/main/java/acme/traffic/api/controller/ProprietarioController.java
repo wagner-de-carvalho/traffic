28a8d46a82dfe13624cc9203bc68ae9fefe707be
wagner <wagncarv@outlook.com>
2024-07-09 14:50:25 -0300

    feature: implementing get proprietario

--- src/main/java/acme/traffic/api/controller/ProprietarioController.java
+++ src/main/java/acme/traffic/api/controller/ProprietarioController.java
@@ -7,19 +7,29 @@ import org.springframework.web.bind.annotation.RestController;
 
 import acme.traffic.domain.model.Proprietario;
 import acme.traffic.domain.repository.ProprietarioRepository;
+import jakarta.websocket.server.PathParam;
 import lombok.AllArgsConstructor;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @AllArgsConstructor
 @RequestMapping("/proprietarios")
 @RestController
 public class ProprietarioController {
 
-    private final ProprietarioRepository proprietarioRepository;;
+    private final ProprietarioRepository proprietarioRepository;
 
     @GetMapping
     public List<Proprietario> listar() {
         return proprietarioRepository.findAll();
+    }
+
+    @GetMapping("/{proprietarioId}")
+    public ResponseEntity<Proprietario> buscar(@PathVariable Long proprietarioId) {
+        return proprietarioRepository.findById(proprietarioId)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 }
