4df6c2b0f3e5e12a24a3ad90fbbc33b747023d18
wagner <wagncarv@outlook.com>
2024-07-09 15:46:12 -0300

    feature: implementing delete proprietario

--- src/main/java/acme/traffic/api/controller/ProprietarioController.java
+++ src/main/java/acme/traffic/api/controller/ProprietarioController.java
@@ -14,6 +14,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -56,4 +57,14 @@ public class ProprietarioController {
         }
         return ResponseEntity.notFound().build();
     }
+
+    @DeleteMapping("/{proprietarioId}")
+    public ResponseEntity<Void> remover(@PathVariable Long proprietarioId) {
+        if (proprietarioRepository.existsById(proprietarioId)) {
+            proprietarioRepository.deleteById(proprietarioId);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+
 }
