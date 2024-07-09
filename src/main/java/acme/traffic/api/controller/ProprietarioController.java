b06107b99ea76d7404dfcac2a68a5c92aa55054c
wagner <wagncarv@outlook.com>
2024-07-09 15:01:41 -0300

    feature: implementing endpoint create proprietario

--- src/main/java/acme/traffic/api/controller/ProprietarioController.java
+++ src/main/java/acme/traffic/api/controller/ProprietarioController.java
@@ -3,6 +3,7 @@ package acme.traffic.api.controller;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import acme.traffic.domain.model.Proprietario;
@@ -10,9 +11,13 @@ import acme.traffic.domain.repository.ProprietarioRepository;
 import jakarta.websocket.server.PathParam;
 import lombok.AllArgsConstructor;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @AllArgsConstructor
 @RequestMapping("/proprietarios")
@@ -31,5 +36,11 @@ public class ProprietarioController {
         return proprietarioRepository.findById(proprietarioId)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Proprietario adicionar(@RequestBody Proprietario proprietario) {
+        return proprietarioRepository.save(proprietario);
     }
 }
