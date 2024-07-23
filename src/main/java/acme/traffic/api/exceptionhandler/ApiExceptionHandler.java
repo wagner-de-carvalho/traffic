b248f22ffe9fd221010357b006324c3468988fc3
wagner carvalho <wagncarv@outlook.com>
2024-07-22 23:43:08 -0300

    feature: using RFC 7807 (Problem Details for HTTP APIs)

--- src/main/java/acme/traffic/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/acme/traffic/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,13 +1,14 @@
-package acme.traffic.domain;
+package acme.traffic.api.exceptionhandler;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import acme.traffic.domain.exception.NegocioException;
 
 @RestControllerAdvice
-public class ApiExceptionHandler {
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(NegocioException.class)
     public ResponseEntity<String> capturar(NegocioException e) {
         return ResponseEntity.badRequest().body(e.getMessage());
