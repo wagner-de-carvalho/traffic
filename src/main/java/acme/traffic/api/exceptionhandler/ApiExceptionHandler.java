a7b1e986a9ef3fd2cf4d81f681d7e681c971e171
wagner carvalho <wagncarv@outlook.com>
2024-07-22 23:53:11 -0300

    feature: customizing ProblemDetail information

--- src/main/java/acme/traffic/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/acme/traffic/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,8 +1,15 @@
 package acme.traffic.api.exceptionhandler;
 
+import java.net.URI;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import acme.traffic.domain.exception.NegocioException;
@@ -12,6 +19,15 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(NegocioException.class)
     public ResponseEntity<String> capturar(NegocioException e) {
         return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+            HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        var problemDetail = ProblemDetail.forStatus(status);
+        problemDetail.setTitle("Um ou mais campos estão inválidos");
+        problemDetail.setType(URI.create("https:/traffic.com/errors/invalid-fields"));
+        return handleExceptionInternal(ex, problemDetail, headers, status, request);
     }
     
 }
