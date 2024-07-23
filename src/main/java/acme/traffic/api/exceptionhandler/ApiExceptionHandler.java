ac951ca8304ff404760e18a7ba9999f44f83348d
wagner carvalho <wagncarv@outlook.com>
2024-07-23 00:07:41 -0300

    feature: add customized fields to ProblemDetail

--- src/main/java/acme/traffic/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/acme/traffic/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,11 +1,15 @@
 package acme.traffic.api.exceptionhandler;
 
 import java.net.URI;
+import java.util.Map;
+import java.util.stream.Collectors;
 
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -22,12 +26,20 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     }
 
     @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException exception,
             HttpHeaders headers, HttpStatusCode status, WebRequest request) {
         var problemDetail = ProblemDetail.forStatus(status);
         problemDetail.setTitle("Um ou mais campos estão inválidos");
         problemDetail.setType(URI.create("https:/traffic.com/errors/invalid-fields"));
-        return handleExceptionInternal(ex, problemDetail, headers, status, request);
+
+        Map<String, String> fields = exception.getBindingResult().getAllErrors()
+                .stream()
+                .collect(Collectors.toMap(objectError -> ((FieldError) objectError).getField(),
+                        DefaultMessageSourceResolvable::getDefaultMessage));
+
+        problemDetail.setProperty("fields", fields);
+
+        return handleExceptionInternal(exception, problemDetail, headers, status, request);
     }
-    
+
 }
